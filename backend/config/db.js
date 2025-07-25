88b14281360eb3bf41b3c93086a4345a753fdf2d
Manmohit Singh <138984093+manmohitsingh23@users.noreply.github.com>
2025-07-25 06:27:27 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://manmohitsingh104:fooddelproject2024@food-del.lek2j.mongodb.net/Food-Del')
+    await mongoose.connect(MONGODB_URI)
     .then(()=>{
         console.log('DB connected');
     })
